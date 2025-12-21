a0f547aea0dd4b366391e787043b4f92d0af7bd2
ll887854 <gtasestacik@gmail.com>
2025-12-21 14:28:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,18 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Goon Finder',
-  description: 'NSFW Reverse Search Tool',
+  description: 'Free NSFW Reverse Image Search Tool',
+  other: {
+    'google-site-verification':
+      'Ky-6wCy-Vr5BmW7Q77GZzzDOJGnVHS_3_nuhy8MqWP0',
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="dark">
       <body>{children}</body>
