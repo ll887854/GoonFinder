b4daf1d70be45ca015d87309315b651d644f7ece
ll887854 <gtasestacik@gmail.com>
2025-12-21 17:34:14 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        {/* ExoClick Ad Provider Loader */}
+        {/* ExoClick Ad Provider */}
         <script async type="application/javascript" src="https://a.magsrv.com/ad-provider.js"></script>
-
-        {/* Your verification tags if you have them */}
-        <meta name="6a97888e-site-verification" content="be2f66aa3a2612f2fec6d8f68b8f1eea" />
       </head>
       <body>{children}</body>
     </html>
